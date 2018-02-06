e7284619c8a6fef178d409a393874790a4cff88d
SmartWalle <smartwalle@gmail.com>
2018-02-06 17:09:34 +0800

    GetDisputeDetails

--- dispute.go
+++ dispute.go
@@ -10,3 +10,10 @@ func (this *PayPal) GetDisputeList(param *DisputeListParam) (results *DisputeLis
 	err = this.doRequestWithAuth("GET", api, nil, &results)
 	return results, err
 }
+
+// GetDisputeDetails https://developer.paypal.com/docs/api/customer-disputes/#disputes_get-dispute
+func (this *PayPal) GetDisputeDetails(disputeId string) (results *Dispute, err error) {
+	var api = this.BuildAPI(k_DISPUTES_API, disputeId)
+	err = this.doRequestWithAuth("GET", api, nil, &results)
+	return results, err
+}
--- dispute_test.go
+++ dispute_test.go
@@ -1,18 +1,25 @@
 package paypal
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestPayPal_GetDisputeList(t *testing.T) {
-	var param = &DisputeListParam{}
-	param.PageSize = 10
-	var results, err = paypal.GetDisputeList(param)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for _, dispute := range results.Items {
-		fmt.Println(dispute.DisputeId, dispute.Status)
-	}
+	//var param = &DisputeListParam{}
+	//param.PageSize = 10
+	//var results, err = paypal.GetDisputeList(param)
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
+	//for _, dispute := range results.Items {
+	//	fmt.Println(dispute.DisputeId, dispute.Status)
+	//}
+}
+
+func TestPayPal_GetDisputeDetails(t *testing.T) {
+	//var results, err = paypal.GetDisputeDetails("PP-000-042-621-836")
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
+	//fmt.Println(results.DisputeId, results.DisputedTransactions[0].InvoiceNumber)
 }
